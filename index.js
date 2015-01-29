2530d712ab0c7a86a59529d08b388799d943bdb0
ryota horiguchi <ryotanoaddress@gmail.com>
2015-01-29 18:32:35 +0900

    Small update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bigquery = gcloud.bigquery({
 var query = 'SELECT year, month, day, thunder, rain, FROM [publicdata:samples.gsod] WHERE thunder=TRUE AND rain=TRUE AND station_number=037720 ORDER BY year DESC, month DESC, day DESC LIMIT 10';
 
 bigquery.query(query, function(err, rows, nextQuery) {
-    console.log(rows);
+    console.log(JSON.stringify(rows));
     if (nextQuery) {
         bigquery.query(nextQuery, function(err, rows, nextQuery) {});
     }
