b46bc5ae74f3de358b19d225a129d3545e230d31
ryota horiguchi <ryotanoaddress@gmail.com>
2015-02-01 14:56:29 +0900

    Small update

--- routes/bq.js
+++ routes/bq.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var config = require('../specific/config.json');
 var gcloud = require('gcloud');
 
-var bigquery = gcloud.bigquery({
-    projectId: config.projectId,
-    keyFilename: config.keyFilename
-});
+var bigquery;
+if (process.env.NODE_ENV === 'staging') {
+    bigquery = gcloud.bigquery({
+        projectId: process.env.PROJECT_ID,
+        credentials: process.env.KEY_FILE
+    });
+} else {
+    var config = require('../specific/config.json');
+    bigquery = gcloud.bigquery({
+        projectId: config.PROJECT_ID,
+        keyFilename: config.KEY_FILE
+    });
+}
 
 router.use(function(req, res, next) {
     console.log('Something is happening.');
@@ -21,7 +29,10 @@ router.get('/', function(req, res) {
 router.get('/url', function(req, res) {
     var query = 'SELECT url FROM [publicdata:samples.github_nested] LIMIT 100';
     bigquery.query(query, function(err, rows, nextQuery) {
-        // console.log(JSON.stringify(rows));
+        if (err) {
+            console.log('err::', err);
+        }
+        console.log(JSON.stringify(rows));
         if (nextQuery) {
             bigquery.query(nextQuery, function(err, rows, nextQuery) {});
         }
@@ -32,7 +43,10 @@ router.get('/url', function(req, res) {
 router.get('/weather', function(req, res) {
     var query = 'SELECT year, month, day, thunder, rain, FROM [publicdata:samples.gsod] WHERE thunder=TRUE AND rain=TRUE AND station_number=037720 ORDER BY year DESC, month DESC, day DESC LIMIT 10';
     bigquery.query(query, function(err, rows, nextQuery) {
-        // console.log(JSON.stringify(rows));
+        if (err) {
+            console.log('err::', err);
+        }
+        console.log(JSON.stringify(rows));
         if (nextQuery) {
             bigquery.query(nextQuery, function(err, rows, nextQuery) {});
         }
